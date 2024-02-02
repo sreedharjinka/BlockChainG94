1f09142adc997835f89fe67f7febad423c779af9
sreedharjinka <98019103+sreedharjinka@users.noreply.github.com>
2024-02-01 21:33:50 -0800

    Update hardhat.config.js

--- frontend/hardhat.config.js
+++ frontend/hardhat.config.js
@@ -1,18 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();                    //Install -- npm install dotenv
-/** @type import('hardhat/config').HardhatUserConfig */
 
-const SEPOLIA_URL=process.env.SEPOLIA_URL;
-const PRIVATE_KEY=process.env.PRIVATE_KEY;
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  networks:{
-    sepolia:{
-      url: SEPOLIA_URL,
-      accounts:[PRIVATE_KEY],
-    },
+  paths: {
+    artifacts: './src/artifacts',
   },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  }
 };
-
-
-//To run sepolia network---  npx hardhat run --network sepolia scripts/finalDeploy.js
